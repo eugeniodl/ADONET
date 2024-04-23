ad359ac8665162477a8c7bc9c5b88c82f86f396c
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-04-23 15:24:05 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo01/Program.cs
@@ -0,0 +1,15 @@
+﻿using Ejemplo01;
+using System.Configuration;
+
+string sqlServerConnectionString =
+    ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+
+IRepository<Customer> repository = 
+    new CustomerRepository(sqlServerConnectionString);
+
+var sqlServerCustomers = repository.GetAll();
+Console.WriteLine("Clientes de SQL Server");
+foreach (var customer in sqlServerCustomers)
+    Console.WriteLine($"ID: {customer.Id}, " +
+        $"Nombre: {customer.FirstName}, " +
+        $"Apellido: {customer.LastName}");
