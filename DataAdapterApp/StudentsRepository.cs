5be7c4b05811aa23572ab42215e5ad1eb1a60f55
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-04-30 10:34:09 -0600

    Completando DataAdapterApp

---
+++ DataAdapterApp/StudentsRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAdapterApp
+{
+    public class StudentsRepository : IStudentsRepository
+    {
+        private readonly string _connectionString;
+
+        public StudentsRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public DataSet GetStudentsDataSet()
+        {
+            using(SqlConnection sqlConnection 
+                = new SqlConnection(_connectionString))
+            {
+                string commandString = "SELECT * FROM Students";
+                using(SqlCommand command 
+                    = new SqlCommand(commandString, sqlConnection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataSet dataSet = new DataSet();
+                    adapter.Fill(dataSet, "Students");
+                    return dataSet;
+                }
+            }
+        }
+
+        public void UpdateStudentsDataSet(DataSet dataSet)
+        {
+            using(SqlConnection sqlConnection =
+                new SqlConnection(_connectionString))
+            {
+                string commandString = "SELECT * FROM Students";
+                using(SqlCommand command
+                    = new SqlCommand(commandString, sqlConnection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    SqlCommandBuilder sqlCommandBuilder =
+                        new SqlCommandBuilder(adapter);
+                    adapter.Update(dataSet, "Students");
+                }
+            }
+        }
+    }
+}
